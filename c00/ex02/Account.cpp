24f1acae208087c7d96ef5b7731d813ee05108e3
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-08-14 18:39:17 +0200

    completed ex2

--- c00/ex02/Account.cpp
+++ c00/ex02/Account.cpp
@@ -1,66 +1,119 @@
-// *****************************************************************************//
-//                                                                               //
-//                Account.hpp for GlobalBanksters United                          //
-//                Created on  : Sun Aug 14 14:04:15 1989                           //
-//                Last update : Sun Aug 14 14:04:06 1992                          //
-//                Made by : Huy "Lightning" Duong <qduong@students.42wolfsburg.de//
-//                                                                              //
+   // *****************************************************************************//
+  //                                                                              //
+ //                Account.hpp for GlobalBanksters United                      //
+//////            Created on  : Sun Aug 14 14:04:15 2069                      ////////
+   //                Last update : Sun Aug 14 18:35:06 2069                          //
+  //                Made by : Huy "Flash" Duong <qduong@students.42wolfsburg.de    //
+ //                                                                              //
 // ****************************************************************************//
 
 #include "Account.hpp"
+#include <iostream>
+#include <string>
 
-static int _nbAccounts = 0;
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
+//"default" constructor
 Account::Account(){
-
+	Account::_nbAccounts += 1;
 }
-
-Account::Account(int initial_deposit){
-	this->_amount = initial_deposit;
+//overloading "default" constructor
+Account::Account(int initial_deposit):_accountIndex(Account::_nbAccounts),
+									 _amount(initial_deposit), 
+									  _nbDeposits(0), 
+									  _nbWithdrawals(0) 
+									  {
+	Account::_nbAccounts += 1;
+	Account::_totalAmount += this->_amount;
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex
+			  <<";amount:" << this->_amount;
+	std::cout << ";created" << std::endl;
 	return ;
 }
 
+// no operator << matches these operands -> #include <string>!
 Account::~Account(){
-
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex
+			  << ";amount:" << this->checkAmount();
+	std::cout << ";closed" << std::endl;
 }
 
+/**
+ * cpp d00 11 non member attributes
+ * this pointer not passed
+ * static makes non member function
+ * @return int notal nb Accounts
+ */
 int Account::getNbAccounts(){
-	return (_nbAccounts);
+	return (Account::_nbAccounts);
 }
 
 int Account::getTotalAmount(){
-	return (_totalAmount);
+	return (Account::_totalAmount);
 }
-
 int Account::getNbDeposits(){
-	return (_totalNbDeposits);
+	return (Account::_totalNbDeposits);
 }
 
 int Account::getNbWithdrawals(){
-	return (_totalNbWithdrawals);
+	return (Account::_totalNbWithdrawals);
 }
 
 void Account::displayAccountsInfos(){
-//prints <<timestamp<< " " << "accounts:" << account_num << ";total:" << total
-		 //<<";deposits:" << deposit << ";withdrawals:" << withdrawals <<std::endl;
+	Account::_displayTimestamp();
+	std::cout << " accounts:" << Account::getNbAccounts() << ";total:" << Account::getTotalAmount()
+			  << ";deposits:" << Account::getNbDeposits() << ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
 }
 
 void Account::makeDeposit(int deposit){
-
+	this->Account::_totalNbDeposits += 1;
+	this->_nbDeposits += 1;
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->checkAmount()
+			  << ";deposit:" << deposit;
+	this->_amount += deposit;
+	this->_totalAmount += deposit;
+	std::cout << ";amount:" << this->checkAmount() << ";nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal){
-
+	if (this->checkAmount() < withdrawal)
+	{
+		Account::_displayTimestamp();
+		std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->checkAmount();
+		std::cout << ";withdrawal:refused" << std::endl;
+		return (0);
+	}
+	else
+	{
+		Account::_displayTimestamp();
+		std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->checkAmount();
+		std::cout << ";withdrawal:" << withdrawal;
+		this->_amount -= withdrawal;
+		this->_totalAmount -= withdrawal;
+		this->_nbWithdrawals += 1;
+		this->Account::_totalNbWithdrawals += 1;
+		std::cout << ";amount:" << this->checkAmount() << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+		return (1);
+	}
 }
 
-int Account::checkAmount(){
-return (this->getTotalAmount);
+int Account::checkAmount()const{
+	return (this->_amount);
 }
 
-int Account::displayStatus(){
-
+void Account::displayStatus()const{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->checkAmount()
+			  << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+	return ;
 }
 
-int Account::_displayTimestamp(){
-	std::cout << std::time(nullptr);
+void Account::_displayTimestamp(){
+	std::cout << "[" << std::time(nullptr) << "]";
 }
