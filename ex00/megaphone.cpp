c538f62769d1eeeec678d4e6ef235187a69f5ba5
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-08-15 10:42:58 +0200

    deleted putchar, doesnt compile on linux, implemented std::string and str.size -> changed implementation to be more c++

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: qduong <qduong@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 15:46:10 by qduong            #+#    #+#             */
-/*   Updated: 2022/08/14 13:19:02 by qduong           ###   ########.fr       */
+/*   Updated: 2022/08/15 10:42:07 by qduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int main(int argc, char** argv)
 		int i = 1;
 		while (argv[i])
 		{
-			//can also declare a string using std::string str(argv[i]);
-			for (size_t x = 0; x < strlen(argv[i]); x++)
-				putchar(toupper(argv[i][x]));
+			std::string str(argv[i]);
+			for (size_t x = 0; x < str.size(); x++)
+				std::cout << (char)(toupper(argv[i][x]));
 			i ++;
 		}
 		std::cout << std::endl;
